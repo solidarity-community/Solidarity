7060b63551dde146a48d4f4ce325f21e4b140edf
arshia11d <arshia11d@outlook.com>
2022-05-15 16:21:05 +0200

    Add `__typeName__` in C# models

--- packages/Server/Domain/Models/Model.cs
+++ packages/Server/Domain/Models/Model.cs
@@ -2,6 +2,8 @@ namespace Solidarity.Domain.Models;
 
 public abstract class Model
 {
+	[System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "IDE1006", Justification = "This is an indicator for the client to be able to identify the type of the model")]
+	public string __typeName__ => GetType().Name;
 	public int Id { get; set; }
 	public int? CreatorId { get; set; }
 	public DateTime Creation { get; set; }
