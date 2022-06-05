c500b842286ab618a71a7b80c11c13a462ad38c0
arshia11d <arshia11d@outlook.com>
2022-06-05 16:22:47 +0200

    Only work with enabled payment methods

--- packages/Server/Infrastructure/Payment/PaymentMethodProvider.cs
+++ packages/Server/Infrastructure/Payment/PaymentMethodProvider.cs
@@ -3,24 +3,19 @@ namespace Solidarity.Infrastructure.Payment;
 public class PaymentMethodProvider : IPaymentMethodProvider
 {
 	private readonly IEnumerable<PaymentMethod> _paymentMethods;
+
 	public PaymentMethodProvider(IServiceProvider serviceProvider)
 	{
+		var enabledPaymentMethods = Environment.GetEnvironmentVariable("PAYMENT_METHODS")?.Split(',');
 		_paymentMethods = typeof(PaymentMethod).Assembly
 			.GetTypes()
-			.Where(t =>
-				t.IsSubclassOf(typeof(PaymentMethod))
-				&& t.GetCustomAttribute<PaymentMethodAttribute>() != null
-			).Select(t => (PaymentMethod)ActivatorUtilities.CreateInstance(serviceProvider, t));
+			.Where(t => t.IsSubclassOf(typeof(PaymentMethod)) && t.GetCustomAttribute<PaymentMethodAttribute>() != null)
+			.Select(t => (PaymentMethod)ActivatorUtilities.CreateInstance(serviceProvider, t))
+			.Where(p => enabledPaymentMethods?.Contains(p.Identifier) ?? false);
 	}
+
+	public IEnumerable<PaymentMethod> GetAll() => _paymentMethods;
 
 	public PaymentMethod Get(string Identifier)
-	{
-		return _paymentMethods.First(pm => pm.Identifier == Identifier)
-			?? throw new InvalidOperationException();
-	}
-
-	public IEnumerable<PaymentMethod> GetAll()
-	{
-		return _paymentMethods;
-	}
+		=> _paymentMethods.First(pm => pm.Identifier == Identifier) ?? throw new InvalidOperationException();
 }
