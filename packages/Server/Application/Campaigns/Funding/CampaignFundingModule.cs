23669709f20e848c272ff8c8c8bcd963d2838f7c
a11delavar <a11delavar@outlook.com>
2024-12-29 04:18:06 +0100

    infra: Bitcoin testnet node

--- packages/Server/Application/Campaigns/Funding/CampaignFundingModule.cs
+++ packages/Server/Application/Campaigns/Funding/CampaignFundingModule.cs
@@ -10,7 +10,7 @@ public sealed class CampaignFundingModule : Module
 
 	public override void ConfigureEndpoints(IEndpointRouteBuilder endpoints)
 	{
-		endpoints.MapGet("/api/campaign/{id}/balance", [AllowAnonymous] async (IDatabase database, IPaymentMethodProvider paymentMethodProvider, int id) => (await database.Get<Campaign>(id)).GetTotalBalance(paymentMethodProvider));
+		endpoints.MapGet("/api/campaign/{id}/balance", [AllowAnonymous] async (IDatabase database, IPaymentMethodProvider paymentMethodProvider, int id) => await (await database.Get<Campaign>(id)).GetTotalBalance(paymentMethodProvider));
 		endpoints.MapGet("/api/campaign/{id}/share", [AllowAnonymous] (GetCampaignBalanceShare getCampaignBalanceShare, int id) => getCampaignBalanceShare.Execute(id));
 		endpoints.MapGet("/api/campaign/{id}/donation-data", [AllowAnonymous] (GetCampaignDonationData getCampaignDonationData, int id) => getCampaignDonationData.Execute(id));
 	}
